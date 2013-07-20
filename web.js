e2cac75e2568c183444342c8a13abfdd4ed590f4
Jorge Chavez <jorgechavez77@gmail.com>
2013-07-20 06:31:54 +0000

    to add media

--- web.js
+++ web.js
@@ -8,11 +8,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+console.log("dir_name: " + __dirname);
+
+app.use(express.static(__dirname + '/videos/'));
+app.use(express.static(__dirname + '/images/'));
+
+var vidStreamer = require("vid-streamer");
+
+app.get('videos/', vidStreamer);
+
 app.get('/', function(request, response) {
   response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
